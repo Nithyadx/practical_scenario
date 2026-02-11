145f44cbbba06fabd92fc6602f539dca6b917b8a
Nithyadx <nithyadas2806@.com>
2026-02-11 14:44:39 +0530

    Intial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Application running successfully - Version 1");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+
